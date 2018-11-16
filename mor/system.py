fa5001ec7cdc39d10a18d0cb432e370a3d475faf
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2018-11-16 00:44:32 -0700

    More work on system class

--- mor/system.py
+++ mor/system.py
@@ -12,6 +12,7 @@ except:
 from scipy.sparse.linalg import eigs, spsolve, LinearOperator
 from scipy.linalg import eig, expm, block_diag, lu_factor, lu_solve, eigvals
 from scipy.sparse import eye as speye
+from scipy.sparse import diags as spdiag
 from scipy.sparse import block_diag as spblock_diag
 from scipy.sparse import issparse, csr_matrix, csc_matrix
 from scipy.optimize import minimize
@@ -21,7 +22,7 @@ from copy import deepcopy
 
 import matplotlib.pyplot as plt
 
-__all__ = ['LTISystem', 'ComboSystem', 'StateSpaceSystem', 'SparseStateSpaceSystem', 'TransferSystem', 'EmptySystem', 'PoleResidueSystem']
+__all__ = ['LTISystem', 'ComboSystem', 'StateSpaceSystem', 'SparseStateSpaceSystem', 'TransferSystem', 'PoleResidueSystem']
 
 
 class LTISystem(object):
@@ -258,7 +259,7 @@ class TransferSystem(LTISystem):
 	def __init__(self, transfer, transfer_der = None, input_dim = 1, output_dim = 1, isreal = False, lim_zH = None,
 			vectorized = False):
 
-		if self.lim_zH is not None:
+		if lim_zH is not None:
 			self._lim_zH = [np.array(lim_zH[0]).reshape(output_dim, input_dim), np.array(lim_zH[1]).reshape(output_dim, input_dim)]
 		else:
 			self._lim_zH = None
@@ -282,7 +283,8 @@ class TransferSystem(LTISystem):
 	def lim_zH(self):
 		if self._lim_zH is None:
 			raise NotImplementedError
-		return self._lim_zH
+		else:
+			return self._lim_zH
 
 	def _transfer(self, z, der = False):
 		n = len(z)
@@ -344,31 +346,43 @@ class StateSpaceSystem(LTISystem):
 	"""
 
 	def __init__(self, A, B, C):
-		self._A = np.array(A)
-		self._B = np.array(B)
-		self._C = np.array(C)
-		
+		try:
+			self._A = A.todense()
+		except:
+			self._A = np.array(A)
+		try:
+			self._B = B.todense()
+		except:
+			self._B = np.array(B)
 
-		if len(self.B_.shape) == 1:
+		try:
+			self._C = C.todense()
+		except:
+			self._C = np.array(C)
+		
+		if len(B.shape) == 1:
 			self._B = self.B_.reshape(-1, 1)
-		if len(self.C_.shape) == 1:
+		if len(C.shape) == 1:
 			self._C = self.C_.reshape(1, -1)
 
 	@property
 	def A(self):
+		""" State space matrix of size (n,n)
+		"""
 		return self._A
 
 	@property
 	def B(self):
+		""" Input matrix of size (n, input_dim)
+		"""
 		return self._B
 
 	@property
 	def C(self):
+		""" Output matrix of size (output_dim, n)
+		"""
 		return self._C
 
-	@property
-	def E(self):
-		return self._E
 
 	@property
 	def state_dim(self):
@@ -384,151 +398,54 @@ class StateSpaceSystem(LTISystem):
 
 	@property
 	def lim_zH(self):
-		if self.E is None:
-			lim_zH = np.dot(self.C, self.B)
-		else:
-			lim_zH = np.dot(self.C, spsolve(self.E, self.B))
-		return complex(lim_zH)
+		#TODO: Check this is valid for complex systems as well
+		lim_zH = np.dot(self.C, self.B)
+		return [lim_zH, lim_zH]
 
-	def pole_box(self):
-		"""Compute the box in which the poles of the system live
-			using the numerical range
-		"""
-
-		real = [None, None]
-		imag = [None, None]
-
-		use_sparse = issparse(self.A) 
-		if self.E is not None:
-			use_sparse = use_sparse & issparse(self.E)
-			
-		if use_sparse:
-			if self.E is not None:
-				A_herm = LinearOperator(self.A.shape, 
-					matvec = lambda x: 0.5*spsolve(self.E, self.A.dot(x)) + 0.5*self.A.T.conj().dot(spsolve(self.E.conj().T, x))
-					)
-				A_skew = LinearOperator(self.A.shape, 
-					matvec = lambda x: 0.5j*spsolve(self.E, self.A.dot(x)) - 0.5j*self.A.T.conj().dot(spsolve(self.E.conj().T, x))
-					)
-			else:
-				A_herm = 0.5 * (self.A + self.A.T.conjugate())
-				A_skew = 0.5j * (self.A - self.A.T.conjugate())
-			
-			# Compute the largest real part of the hermitian part (right most limit)	
-			ew, ev = eigs(A_herm, k=1, which='LR')
-			real[1] = float(ew.real)
-			# Compute the smallest real part of the hermitian part (left most limit)	
-			ew, ev = eigs(A_herm, k=1, which='SR')
-			real[0] = float(ew.real)
-
-			# Compute the largest real part of the skew-hermitian part (top most limit)	
-			ew, ev = eigs(A_skew, k=1, which='LR')
-			imag[1] = float(ew.real)
-			# Compute the smallest real part of the skew-hermitian part (bottom most limit)	
-			ew, ev = eigs(A_skew, k=1, which='SR')
-			imag[0] = float(ew.real)
-
-		else:
-			if self.E is not None:
-				if issparse(self.A):
-					A = self.A.todense()
-				else:
-					A = self.A
-				if issparse(self.E):
-					E = self.E.todense()
-				else:
-					E = self.E
-
-				A = solve(E, A)
-				A_herm = 0.5*(A + A.conj().T)
-				A_skew = 0.5j*(A - A.conj().T)
-			else:
-				A_herm = 0.5*(self.A + self.A.conj().T)
-				A_skew = 0.5j*(self.A - self.A.conj().T)
-			
-			ew = eig(A_herm, left=False, right=False)
-			real[1] = np.max(ew.real)
-			real[0] = np.min(ew.real)
-
-			ew = eig(A_skew, left=False, right=False)
-			imag[0] = np.min(ew.real)
-			imag[1] = np.max(ew.real)
-
-		return real, imag
 
 	def norm(self):
-		if self.E is not None:
-			raise NotImplementedError
 
 		if self.spectral_abscissa() >= 0:
 			return np.inf
+
 		# Replace with code that exploits Q is rank-1 and sparse structure for A
-		Q = -np.outer(self.B, self.B.conjugate())
-		if issparse(self.A):
-			A = self.A.todense()
+		norm2 = 0
+		for i in range(self.input_dim):
+			for j in range(self.output_dim):
+				Q = -np.outer(self.B[:,i], self.B[:,i].conjugate())
+				X = solve_lyapunov(self.A, Q)
+
+				norm2 += np.dot(self.C[j,:], np.dot(X, self.C[j,:].conjugate().T))
+		return np.sqrt(norm2.real)
+
+#	def impulse(self, t):
+#		if self.E is not None:
+#			raise NotImplementedError
+#
+#		output = np.dot(self.C, np.dot(expm(t * self.A), self.B))
+#		return output.reshape(self.output_dim, self.input_dim)
+
+	def _transfer(self, z, der = False):
+		n = len(z)
+
+		Hz = np.zeros((n, self.output_dim, self.input_dim), dtype = np.complex)
+		if der:
+			Hpz = np.zeros((n, self.output_dim, self.input_dim), dtype = np.complex)
+		
+		I = eye(self.A.shape[0])
+		for i in range(n):
+			x = solve(I*z - self.A, self.B)
+			Hz[i,:,:] = np.dot(self.C, x)
+			if der:
+				IA = z * I - self.A
+				x_der = solve(IA, x)
+				Hpz[i,:,:] = np.dot(-self.C, x_der)
+
+		if der:
+			return Hz, Hpz
 		else:
-			A = self.A
-		X = solve_lyapunov(A, Q)
-		#np.savetxt('norm_%d_A.dat' % A.shape[0], A.view(float))
-		#np.savetxt('norm_%d_B.dat' % A.shape[0], self.B.view(float))
-		#np.savetxt('norm_%d_C.dat' % A.shape[0], self.C.view(float))
-		#np.savetxt('norm_%d_X.dat' % A.shape[0], X.view(float))
+			return Hz
 
-		pre_norm = np.dot(self.C, np.dot(X, self.C.conjugate().T))
-		norm = np.sqrt(np.dot(self.C, np.dot(X, self.C.conjugate().T)))
-		return float(norm.real)
-
-	def impulse(self, t):
-		if self.E is not None:
-			raise NotImplementedError
-
-		output = np.dot(self.C, np.dot(expm(t * self.A), self.B))
-		return output.reshape(self.output_dim, self.input_dim)
-
-	def transfer(self, z):
-		if issparse(self.A):
-			# sparse version
-			if self.E is None:
-				E = csr_matrix(speye(self.state_dim))
-			else:
-				E = self.E
-			EA = z * E - self.A
-			x = spsolve(EA, self.B)
-		else:
-			# dense version
-			if self.E is None:
-				E = eye(self.state_dim)
-			else:
-				E = self.E
-			x = solve(z * E - self.A, self.B)
-
-		output = np.dot(self.C, x)
-		return output.reshape(self.output_dim, self.input_dim)
-
-	def transfer_der(self, z):
-		#TODO: What is E is dense and A is sparse?
-		if issparse(self.A):
-			# sparse version
-			if self.E is None:
-				E = csr_matrix(speye(self.state_dim))
-			else:
-				E = self.E
-			EA = z * E - self.A
-			x = spsolve(EA, self.B)
-			x_der = spsolve(EA, x)
-		else:
-			# dense version
-			if self.E is None:
-				E = eye(self.state_dim)
-			else:
-				E = self.E
-			EA = z * E - self.A
-			x = solve(EA, self.B)
-			x_der = solve(EA, x)
-
-		H_z = np.dot(self.C, x)
-		Hp_z = np.dot(-self.C, x_der)
-		return H_z.reshape(self.output_dim, self.input_dim), Hp_z.reshape(self.output_dim, self.input_dim)
 
 	def __add__(self, other):
 		if self.input_dim != other.input_dim:
@@ -536,44 +453,17 @@ class StateSpaceSystem(LTISystem):
 		if self.output_dim != other.output_dim:
 			raise ValueError("Output dimensions must be the same")
 
-		# Combine A
-		if issparse(self.A) and issparse(other.A):
-			A = spblock_diag([self.A, other.A])
-		elif not issparse(self.A) and issparse(other.A):
-			A = block_diag(self.A, other.A.todense())
-		elif issparse(self.A) and not issparse(other.A):
-			A = block_diag(self.A.todense(), other.A)
-		elif not issparse(self.A) and not issparse(other.A):
-			A = block_diag(self.A, other.A)
-		else:
-			raise NotImplementedError('This should never be called')
+		# By default for now, we convert things that are state-space systems to
+		# dense systems for the combination
+		if isinstance(other, StateSpaceSystem):
+			A = block_diag(self.A, np.array(other.A))
+			B = np.vstack([self.B, other.B])
+			C = np.hstack([self.C, other.C])
+		else: 
+			raise NotImplementedError("Don't know how to combine these systems")
 
-		B = np.vstack([self.B, other.B])
-		C = np.hstack([self.C, other.C])
 
-		# Combine E
-		if self.E is None and other.E is None:
-			E = None
-		elif self.E is None and issparse(other.E):
-			E = spblock_diag([speye(self.state_dim), other.E])
-		elif self.E is None and not issparse(other.E):
-			E = block_diag(np.eye(self.state_dim), other.E)
-		elif issparse(self.E) and other.E is None:
-			E = spblock_diag([self.E, speye(other.state_dim)])
-		elif not issparse(self.E) and other.E is None:
-			E = block_diag(self.E, np.eye(other.state_dim))
-		elif issparse(self.E) and issparse(other.E):
-			E = spblock_diag([self.E, other.E])
-		elif issparse(self.E) and not issparse(other.E):
-			E = block_diag(self.E.todense(), E)
-		elif not issparse(self.E) and issparse(other.E):
-			E = block_diag(self.E, other.E.todense())
-		elif not issparse(self.E) and not issparse(other.E):
-			E = block_diag(self.E, other.E)
-		else:
-			raise NotImplementedError('This should never be called')
-
-		return StateSpaceSystem(A, B, C, E = E)
+		return StateSpaceSystem(A, B, C)
 
 	def __sub__(self, other):
 		if self.input_dim != other.input_dim:
@@ -581,44 +471,14 @@ class StateSpaceSystem(LTISystem):
 		if self.output_dim != other.output_dim:
 			raise ValueError("Output dimensions must be the same")
 
-		# Combine A
-		if issparse(self.A) and issparse(other.A):
-			A = spblock_diag([self.A, other.A])
-		elif not issparse(self.A) and issparse(other.A):
-			A = block_diag(self.A, other.A.todense())
-		elif issparse(self.A) and not issparse(other.A):
-			A = block_diag(self.A.todense(), other.A)
-		elif not issparse(self.A) and not issparse(other.A):
-			A = block_diag(self.A, other.A)
-		else:
-			raise NotImplementedError('This should never be called')
+		if isinstance(other, StateSpaceSystem):
+			A = block_diag(self.A, np.array(other.A))
+			B = np.vstack([self.B, other.B])
+			C = np.hstack([self.C, -1*other.C])
+		else: 
+			raise NotImplementedError("Don't know how to combine these systems")
 
-		B = np.vstack([self.B, other.B])
-		C = np.hstack([self.C, -1.*other.C])
-
-		# Combine E
-		if self.E is None and other.E is None:
-			E = None
-		elif self.E is None and issparse(other.E):
-			E = spblock_diag([speye(self.state_dim), other.E])
-		elif self.E is None and not issparse(other.E):
-			E = block_diag(np.eye(self.state_dim), other.E)
-		elif issparse(self.E) and other.E is None:
-			E = spblock_diag([self.E, speye(other.state_dim)])
-		elif not issparse(self.E) and other.E is None:
-			E = block_diag(self.E, np.eye(other.state_dim))
-		elif issparse(self.E) and issparse(other.E):
-			E = spblock_diag([self.E, other.E])
-		elif issparse(self.E) and not issparse(other.E):
-			E = block_diag(self.E.todense(), E)
-		elif not issparse(self.E) and issparse(other.E):
-			E = block_diag(self.E, other.E.todense())
-		elif not issparse(self.E) and not issparse(other.E):
-			E = block_diag(self.E, other.E)
-		else:
-			raise NotImplementedError('This should never be called')
-
-		return StateSpaceSystem(A, B, C, E = E)
+		return StateSpaceSystem(A, B, C)
 
 	def __mul__(self, other):
 		ret = deepcopy(self)
@@ -637,24 +497,18 @@ class StateSpaceSystem(LTISystem):
 		else:
 			return np.isrealobj(self.A) & np.all(np.isreal(self.B)) & np.all(np.isreal(self.C)) & np.isrealobj(self.E)
 
-	def poles(self):
-		if self.E is not None:
-			raise NotImplementedError
-		if issparse(self.A):
-			ew = eig(self.A.todense(), left=False, right=False)
-		else:
-			ew = eig(self.A, left=False, right=False)
-		return ew
+#	def poles(self):
+#		if self.E is not None:
+#			raise NotImplementedError
+#		if issparse(self.A):
+#			ew = eig(self.A.todense(), left=False, right=False)
+#		else:
+#			ew = eig(self.A, left=False, right=False)
+#		return ew
 
 	def spectral_abscissa(self):
-		if self.E is not None:
-			raise NotImplementedError
-		if issparse(self.A):
-			ew = eigs(self.A, 1, which = 'LR', return_eigenvectors = False)
-			return float(ew.real) 
-		else:
-			ew = eigvals(self.A)
-			return np.max(ew.real)
+		ew = eigvals(self.A)
+		return np.max(ew.real)
 
 
 class SparseStateSpaceSystem(StateSpaceSystem):
@@ -662,30 +516,46 @@ class SparseStateSpaceSystem(StateSpaceSystem):
 		pass
 
 
+	def spectral_abscissa(self):
+		ew = eigs(self.A, 1, which = 'LR', return_eigenvectors = False)
+		return float(ew.real)
+
+	
+
+
 class PoleResidueSystem(StateSpaceSystem):
 	def __init__(self, poles, residues):
-		self._poles = np.copy(poles)
-		self._residues = np.copy(residues)
-		A = np.diag(poles)
-		C = np.copy(residues)
-		B = np.ones(len(poles))
+		self._poles = np.array(poles)
+		n = len(self._poles)
+		# TODO: What about MIMO pole residue systems
+		self._residues = np.array(residues).reshape(n, 1,1)
 
-		# TODO: Option to create real valued A, B, C if poles and residues correspond to real system
-		StateSpaceSystem.__init__(self, A, B, C)
-		# TODO: Evaluate transfer function 
+	@property
+	def A(self):
+		return spdiag(self._poles)
+
+	@property
+	def B(self):
+		return self._residues.reshape(-1,1)
+
+	@property
+	def C(self):
+		return np.ones((1, len(self.poles)))
+
+	@property
+	def poles(self):
+		return self._poles
+
+	@property
+	def spectral_abscissa(self):
+		return np.max(self._poles.real)
 
 
-
-class EmptySystem(StateSpaceSystem):
-	def __init__(self):
-		self.A_ = np.zeros((0, 0))
-		self.B_ = np.zeros((0, 1))
-		self.C_ = np.zeros((1, 0))
 
 
 if __name__ == '__main__':
-	from test_cases import build_cdplayer
+	from demos import build_cdplayer
 
 	model = build_cdplayer()
 	print "H2 Norm = %1.2e" % model.norm()
-	model.bode()
+	print (model - model).norm()
