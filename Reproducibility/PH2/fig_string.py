b4a7630335ed1e477a510e0034b595c8d162fda2
Jeffrey Hokanson <jeffrey@hokanson.us>
2018-11-21 11:27:18 -0700

    Updated examples

--- Reproducibility/PH2/fig_string.py
+++ Reproducibility/PH2/fig_string.py
@@ -1,16 +1,15 @@
 import numpy as np
+import scipy.optimize
 from mor import ProjectedH2MOR, IRKA, TFIRKA, QuadVF
-from mor.demos import build_iss
+from mor.demos import build_string, build_iss
 from mor.pgf import PGF
 
 
 def run_mor(MOR, rs, prefix, **kwargs):
 	rs = np.atleast_1d(rs)
 
-	H = build_iss()
+	H = build_string()
 	# Make a SISO system
-	H = H[0,0]
-
 	H_norm = H.norm()
 
 	rel_err = np.nan*np.zeros(rs.shape)	
@@ -80,16 +79,35 @@ def run_quadvf(Ns, r, L, prefix,  **kwargs):
 	pgf.add('rel_err', err)
 	pgf.write(prefix + '_%02d_L%d.dat' % (r, L))
 
+def choose_L(L_min, L_max, n = 1000):
+	H = build_string()
+	#H = build_iss()
+	#H = H[0,0]
+	norm = lambda L: H.quad_norm(L = L, n = n)
+	for L in np.logspace(-2,2, 100):
+		print L, "\t",  norm(L)
+	#L = scipy.optimize.brute(norm, ranges= [(np.log10(L_min), np.log10(L_max))], Ns= 1e3, finish = None)
+	#print "Quad Norm", -norm(L)
+	#print "Actual Norm", H.norm()
+	return float(10.**L)
+
+
 if __name__ == '__main__':
-	ftol = 1e-9
-	rs = np.arange(2,50+2,2)
-	run_mor(ProjectedH2MOR, rs, 'data/fig_iss_ph2', verbose = True, print_norm = True, cond_growth = 5, ftol = 1e-9, maxiter =200)
-	run_mor(IRKA, rs, 'data/fig_iss_irka', verbose = True, print_norm = True, ftol = ftol)
-	run_mor(TFIRKA, rs, 'data/fig_iss_tfirka', verbose = True, print_norm = True, ftol = ftol)
+	H = build_string(epsilon = 10)
+	for N  in 1e3*np.arange(1,1000):
+		print int(N), '\t', H.quad_norm(L = 100, n = int(N))
+
+	#L = choose_L(1e-2,1e2, n = int(1e5))
+	#L = 5, N = 1e5 seem to be resonable choices
+	
+	#ftol = 1e-9
+	#rs = np.arange(2,50+2,2)
+	#rs = [2]
+	#run_mor(ProjectedH2MOR, rs, 'data/fig_string_ph2', verbose = True, print_norm = True, cond_growth = 5, ftol = 1e-9, maxiter =200)
+	#run_mor(IRKA, rs, 'data/fig_iss_irka', verbose = True, print_norm = True, ftol = ftol)
+	#run_mor(TFIRKA, rs, 'data/fig_string_tfirka', verbose = True, print_norm = True, ftol = ftol)
 
 	#Ns = np.arange(25, 1000+1,1)
 	#Ls = [10]
 	#for L in Ls:
 	#	run_quadvf(Ns, 28, L, 'data/fig_iss_quadvf')
-	
-
