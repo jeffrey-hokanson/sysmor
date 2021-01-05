f1ad208568ef78fe7615fb9a5681f03c493076d5
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2021-01-05 16:33:22 -0700

    explicity computing the inverse

--- sysmor/mimoph2.py
+++ sysmor/mimoph2.py
@@ -33,19 +33,26 @@ def inner_loop(z, Y, r, poles = None):
 	elif len(poles) < r:
 		raise NotImplementedError
 
+	# The z's are the points at which we evaluate the transfer function
+	# in the RHP (also mu's in the text) 
 	weight = Weight(z) 
+	# It seems more expedient to explicitly form the inverse
+	# this is not unreasonable given how many times we apply it.
+	if True:
+		weight = weight @ np.eye(Y.shape[0])
 
 	vf = polyrat.VectorFittingRationalApproximation(r-1, r, poles0 = poles)
 	vf.fit(z.reshape(-1,1), Y)
 	lam, R = vf.pole_residue()
 
 	# Make rank-one approximations of each residue
-	for i in range(R.shape[0]):
-		U, s, VH = scipy.linalg.svd(R[i])
-		R[i] = s[0]*(U[:,0:1] @ VH[0:1,:])
+	#for i in range(R.shape[0]):
+	#	U, s, VH = scipy.linalg.svd(R[i])
+	#	R[i] = s[0]*(U[:,0:1] @ VH[0:1,:])
 
-	# Convert into Real MIMO form
+	# Convert into Real MIMO form with rank-one residues
 	output = pole_residue_to_real_statespace(lam, R, rank = 1)
 
 	# Optimize the fit 
-	output = fit_real_mimo_statespace_system(z, Y, *output, stable = True, weight = weight)	
+	output = fit_real_mimo_statespace_system(z, Y, *output, stable = True, weight = weight)
+	
